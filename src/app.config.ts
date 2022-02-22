06a8ee46c84f8e739007dd3f611c51692bf340bc
Tequila sunset <72564247+DummyNodeHead@users.noreply.github.com>
2022-02-22 16:04:17 +0800

    usercenter first commit

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,22 +1,22 @@
 export default {
   pages: [
-    'pages/index/index',
     'pages/usercenter/usercenter',
+    'pages/index/index',
   ],
   tabBar: {
     list: [{
       'iconPath': 'resource/home.png',
-      'selectedIconPath': 'resource/user.png',
+      'selectedIconPath': 'resource/home_on.png',
       'pagePath': 'pages/index/index',
       'text': '图书商店'
     }, {
       'iconPath': 'resource/user.png',
-      'selectedIconPath': 'resource/home.png',
+      'selectedIconPath': 'resource/user_on.png',
       'pagePath': 'pages/usercenter/usercenter',
       'text': '个人中心'
     }],
     'color': '#000',
-    'selectedColor': '#56abe4',
+    'selectedColor': '#389e0d',
     'backgroundColor': '#fff',
     'borderStyle': 'white'
   },
--- src/pages/usercenter/usercenter.tsx
+++ src/pages/usercenter/usercenter.tsx
@@ -7,53 +7,60 @@ import './usercenter.less'
 function UserCenter() {
 
   return (
-    <View className='gridshow'>
-      <Image className='avatar' src='https://img2.baidu.com/it/u=1537133661,1519623815&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500' />
-      <View className='username'>haha</View>
-      <View className='container1'>
-        <Image className='icon1' src='../../resource/home.png' />
-        <View>我的收藏</View>
-      </View>
+    <View className='bg'>
+      <View className='gridshow'>
+        <View className='container0'>
+          <Image className='avatar' src='https://joeschmoe.io/api/v1/random' />
+          <View className='username'>王小明
+          <View className='line'/>
+          <View className='school'>XXX大学</View>
+          </View>
+        </View>
+        <View className='container1'>
+          <View>
+            <Image className='icon1' src='../../resource/allorder.png' />
+            <View>全部订单</ View>
+          </View>
+          <View>
+            <Image className='icon1' src='../../resource/payment.png' />
+            <View>待付款</View>
+          </View>
 
-      <View className='container1' onClick={test}>
-        <Image className='icon1' src='../../resource/home.png' />
-        <View>个人信息</View>
-      </View>
+          <View>
+            <Image className='icon1' src='../../resource/package.png' />
+            <View>待发货</View>
+          </View>
 
-      <View className='container1'>
-        <Image className='icon1' src='../../resource/home.png' />
-        <View>全部订单</View>
-      </View>
+          <View>
+            <Image className='icon1' src='../../resource/delivery.png' />
+            <View>待收货</View>
+          </View>
 
-      <View className='container1'>
-        <Image className='icon1' src='../../resource/home.png' />
-        <View>图书回收</View>
-      </View>
+          <View>
+            <Image className='icon1' src='../../resource/star.png' />
+            <View>我的收藏</View>
+          </View>
+          <View onClick={test}>
+            <Image className='icon1' src='../../resource/userinfo.png' />
+            <View>个人信息</View>
+          </View>
 
-      <View className='container1'>
-        <Image className='icon1' src='../../resource/home.png' />
-        <View>待付款</View>
-      </View>
+          <View>
+            <Image className='icon1' src='../../resource/recycle.png' />
+            <View>图书回收</View>
+          </View>
 
-      <View className='container1'>
-        <Image className='icon1' src='../../resource/home.png' />
-        <View>待发货</View>
-      </View>
-
-      <View className='container1'>
-        <Image className='icon1' src='../../resource/home.png' />
-        <View>待收货</View>
-      </View>
-
-      <View className='container1'>
-        <Image className='icon1' src='../../resource/home.png' />
-        <View>意见反馈</View>
+          <View>
+            <Image className='icon1' src='../../resource/advice.png' />
+            <View>意见反馈</View>
+          </View>
+        </View>
       </View>
     </View>
   )
 }
 
-const test=()=>{
+const test = () => {
   console.log("test")
 }
 

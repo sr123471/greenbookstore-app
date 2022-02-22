f4df84aee99bbacb9e3b2059d999f9fd6cb69404
Tequila sunset <72564247+DummyNodeHead@users.noreply.github.com>
2022-02-22 19:40:14 +0800

    userinfo version 1

--- src/pages/userinfo/userinfo.tsx
+++ src/pages/userinfo/userinfo.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { Component, ReactNode } from 'react'
 import { View, Text } from '@tarojs/components'
 import { AtList, AtListItem } from "taro-ui"
 
@@ -6,16 +6,30 @@ import "taro-ui/dist/style/components/list.scss";
 
 import './userinfo.less'
 
-function UserInfo() {
 
-  return (
-    <AtList>
-      <AtListItem title='标题文字' />
-      <AtListItem title='标题文字' arrow='right' />
-      <AtListItem title='标题文字' extraText='详细信息' />
-      <AtListItem title='禁用状态' disabled extraText='详细信息' />
-    </AtList>
-  )
+export default class Index extends Component<any, any> {
+
+  state={
+    name:'',
+    sex:'',
+    age:'',
+    school:'',
+    major:'',
+    phone:'',
+  }
+
+  render(): ReactNode {
+    return (
+      <AtList>
+        <AtListItem title='姓名' extraText='王小明' />
+        <AtListItem title='性别' extraText='男' />
+        <AtListItem title='年龄' extraText='18' />
+        <AtListItem title='学校' extraText='xxx大学' />
+        <AtListItem title='专业' extraText='软件工程' />
+        <AtListItem title='电话' extraText='1xxxxxxxxxx' />
+      </AtList>
+      //修改个人信息页面暂时未做
+    )
+  }
+
 }
-
-export default UserInfo

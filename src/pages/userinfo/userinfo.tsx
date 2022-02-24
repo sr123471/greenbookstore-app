a1a1f98fa40b82eec55a57e0d2c6319765faac49
Tequila sunset <72564247+DummyNodeHead@users.noreply.github.com>
2022-02-24 10:19:30 +0800

    background refined

--- src/pages/userinfo/userinfo.tsx
+++ src/pages/userinfo/userinfo.tsx
@@ -19,32 +19,34 @@ export default class Index extends Component<any, any> {
     phone: '',
   }
 
-  toAddrManager(){
-    Taro.navigateTo({url:'../address/address'})
+  toAddrManager() {
+    Taro.navigateTo({ url: '../address/address' })
   }
 
   render(): ReactNode {
     return (
-      <View>
+      <View className='bg'>
         <View className='container0'>
           <View className='title'>个人信息</View>
           <View className='line' />
         </View>
-        <AtList>
-          <AtListItem title='姓名' extraText='王小明' />
-          <AtListItem title='性别' extraText='男' />
-          <AtListItem title='年龄' extraText='18' />
-          <AtListItem title='学校' extraText='xxx大学' />
-          <AtListItem title='专业' extraText='软件工程' />
-          <AtListItem title='电话' extraText='1xxxxxxxxxx' />
-          <AtListItem
-            onClick={this.toAddrManager.bind(this)}
-            title='收货地址'
-            note='管理您的收货地址'
-            arrow='right'
-            thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
-          />
-        </AtList>
+        <View className='info'>
+          <AtList>
+            <AtListItem title='姓名' extraText='王小明' />
+            <AtListItem title='性别' extraText='男' />
+            <AtListItem title='年龄' extraText='18' />
+            <AtListItem title='学校' extraText='xxx大学' />
+            <AtListItem title='专业' extraText='软件工程' />
+            <AtListItem title='电话' extraText='1xxxxxxxxxx' />
+            <AtListItem
+              onClick={this.toAddrManager.bind(this)}
+              title='收货地址'
+              note='管理您的收货地址'
+              arrow='right'
+              thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
+            />
+          </AtList>
+        </View>
       </View>
       //修改个人信息页面暂时未做
     )
